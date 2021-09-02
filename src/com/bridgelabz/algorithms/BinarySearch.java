19dfc679e6f3c0a291da07b4d3d4c0693e36b050
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-02 23:23:28 +0530

    ADDED: Binary Search to find given word in list of words after sorting

---
+++ src/com/bridgelabz/algorithms/BinarySearch.java
@@ -0,0 +1,48 @@
+package com.bridgelabz.algorithms;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class BinarySearch {
+	public static void binarySearch(String listOfWords[], int firstIndex, int lastIndex, String key) {
+		int middleIndex = (firstIndex + lastIndex) / 2;
+
+		while (firstIndex <= lastIndex) {
+			if (listOfWords[middleIndex].compareTo(key) < 0) {
+				firstIndex = middleIndex + 1;
+			} else if (listOfWords[middleIndex].compareTo(key) == 0) {
+				System.out.println("Given word : " + key + " is found at index: " + middleIndex);
+				break;
+			} else {
+				lastIndex = middleIndex - 1;
+			}
+			middleIndex = (firstIndex + lastIndex) / 2;
+		}
+		if (firstIndex > lastIndex) {
+			System.out.println("Given word : " + key + " is not found");
+		}
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Enter the list of words :");
+		Scanner scanner = new Scanner(System.in);
+		String string = scanner.nextLine();
+
+		System.out.println("List of words after sorting:");
+		String listOfWords[] = string.toLowerCase().split(",");
+
+		Arrays.sort(listOfWords);
+
+		for (String word : listOfWords) {
+			System.out.print(word+" ");
+		}
+		System.out.println("\n\nEnter the key word to search");
+		String key = scanner.next();
+
+		Arrays.sort(listOfWords);
+		binarySearch(listOfWords, 0, (listOfWords.length) - 1, key);
+
+	}
+}
